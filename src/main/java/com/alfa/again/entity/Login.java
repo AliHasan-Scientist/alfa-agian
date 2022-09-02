4b6111e09a77ace19a61a170fc69cdf1be8fba5d
Roy Ali Hasan <royalihasan0987@gmail.com>
2022-09-02 09:30:42 +0500

    Postgresql + JPA Dependency => ADDED

---
+++ src/main/java/com/alfa/again/entity/Login.java
@@ -0,0 +1,46 @@
+package com.alfa.again.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "login")
+public class Login {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
